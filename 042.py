5a9b947d424f9644e311c6546329c46c447727f9
Gabriel Valeriano <gr.valeriano98@gmail.com>
2024-06-28 08:58:58 -0300

    Meu segundo commit

--- 042.py
+++ 042.py
@@ -10,7 +10,7 @@ Simulação de um Caixa Eletrônico. Este programa simula um caixa eletrônico, 
 
 print('--- Bem-vindo ao Nubielk ---')
 
-saldo_atual = float(1000000)
+saldo_atual = float(0)
 saldo_poupanca = 0
 escolha = ''
 taxa = 0.01
@@ -25,49 +25,48 @@ while escolha != 7:
                         '\n[7. Sair da Conta]'
                         '\nDigite a sua escolha: '))
     if escolha == 4 and escolha > 0:
-        emprestimo = int(input('Digite o valor de empréstimo: '))
+        emprestimo = float(input('Digite o valor de empréstimo: '))
         if emprestimo < 0:
             print('Operação Inválida. Cód. do erro 171')
         else:
-            print(f'O valor do emprestimo é R${emprestimo:.2f} e sua conta ficará com {saldo_atual + emprestimo:.2f}')
+            print(f'O valor do emprestimo é R${emprestimo:.2f} e sua conta ficará com R${saldo_atual + emprestimo:.2f}')
         saldo_atual += emprestimo
 
 
 
     elif escolha == 1 and escolha > 0:
-        deposito = int(input('Digite o valor do depósito: '))
+        deposito = float(input('Digite o valor do depósito: '))
         if deposito < 0:
             print('Operação Inválida. Cód. do erro 171')
         else:
-            deposito = int(input('Digite o valor do depósito: '))
-            print(f'O valor do depósito é R${deposito:.2f} e sua conta ficará com {saldo_atual + deposito:.2f}')
+            deposito = float(input('Confirme o valor do depósito: '))
+            print(f'O valor do depósito é R${deposito:.2f} e sua conta ficará com R${saldo_atual + deposito:.2f}')
             saldo_atual += deposito
 
     elif escolha == 2:
-        saque = int(input('Digite o valor do saque: '))
+        saque = float(input('Digite o valor do saque: '))
         if saque < 0:
             print('Operação Inválida. Cód. do erro 171')
         else:
-            print(f'O valor do saque é R${saque:.2f} e sua conta ficará com {saldo_atual - saque:.2f}')
+            print(f'O valor do saque é R${saque:.2f} e sua conta ficará com R${saldo_atual - saque:.2f}')
         saldo_atual -= saque
 
-
-
     elif escolha == 5 and escolha > 0:
-        poupança = int(input('Digite o valor para depositar na poupança: '))
+        poupança = float(input('Digite o valor para depositar na poupança: '))
         meses = int(input('Quantos meses pretende deixar rendendo? '))
-        print(f'O valor do depósito na poupança é R${poupança:.2f} e sua poupança ficará com {saldo_poupanca + poupança:.2f}')
-        print(f'O valor de {poupança} em {meses} meses lhe renderá R${(poupança * (1 + taxa) ** meses) - poupança:.2f}')
+        print(f'O valor do depósito na poupança é R${poupança:.2f} e sua poupança ficará com R${saldo_poupanca + poupança:.2f}')
+        print(f'O valor de {poupança:.2f} em {meses} meses lhe renderá R${(poupança * (1 + taxa) ** meses) - poupança:.2f}')
         saldo_poupanca += poupança
         saldo_atual = saldo_atual - saldo_poupanca
-        print(f'O valor atual da sua conta corrente é {saldo_atual}')
+        print(f'O valor atual da sua conta corrente é R${saldo_atual:.2f}')
 
     elif escolha == 6 and escolha > 0:
-        print(f'O seu saldo atual da poupança é R${saldo_poupanca}')
+        print(f'O seu saldo atual da poupança é R${saldo_poupanca:.2f}')
 
     elif escolha == 3:
         print(f'O seu saldo atual é R${saldo_atual:.2f}')
 
-
+    elif escolha > 7:
+        print('Digite novamente.')
     else:
         print('O Nubielk agradece a preferência!')
